e77c2a3f1f6d014e2c654a8d8a9b80d818681266
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-04-04 12:22:43 +0800

    基础练习 01字串

---
+++ 001/002.cpp
@@ -0,0 +1,13 @@
+/* 基础练习 - 01字串 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 32; ++i)
+        cout << (i % 32) / 16 << (i % 16) / 8 << (i % 8) / 4 << (i % 4) / 2 << i % 2 << endl;
+
+    return 0;
+}
