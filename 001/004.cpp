2e61c2e5cc60cdc316723cc529048cb296a9b921
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-04-05 00:07:36 +0800

    基础练习 数列特征

---
+++ 001/004.cpp
@@ -0,0 +1,32 @@
+/* 基础练习 - 数列特征 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int m[n];
+
+    for (int i = 0; i < n; ++i)
+        cin >> m[i];
+
+    int min = m[0], max = m[0], sum = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        sum += m[i];
+
+        if (min > m[i]) min = m[i];
+
+        if (max < m[i]) max = m[i];
+    }
+
+    cout << max << endl << min << endl << sum << endl;
+
+    return 0;
+}
