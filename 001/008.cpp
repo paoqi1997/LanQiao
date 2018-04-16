09c497a136729d13eea66d76dc349f21b8361ec5
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-04-16 17:51:49 +0800

    基础练习 回文数

---
+++ 001/008.cpp
@@ -0,0 +1,26 @@
+/* 基础练习 - 回文数 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x0, x1, x2, x3;
+
+    for (int i = 1000; i <= 9999; ++i)
+    {
+        x3 = i / 1000;
+
+        x2 = (i - (x3 * 1000)) / 100;
+
+        x1 = (i - (x3 * 1000) - (x2 * 100)) / 10;
+
+        x0 = (i - (x3 * 1000) - (x2 * 100) - (x1 * 10));
+
+        if (x0 == x3 && x1 == x2)
+            cout << i << endl;
+    }
+
+    return 0;
+}
