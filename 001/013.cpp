94c1a46c49b0d5f75f63a5f87f9e393d21c805f6
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-04-19 21:49:57 +0800

    基础练习 数列排序

---
+++ 001/013.cpp
@@ -0,0 +1,26 @@
+/* 基础练习 - 数列排序 */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int m[n];
+
+    for (int i = 0; i < n; ++i)
+        cin >> m[i];
+
+    sort(m, m + n);
+
+    for (int i = 0; i < n; ++i) {
+        cout << m[i] << ' ';
+    } cout << endl;
+
+    return 0;
+}
