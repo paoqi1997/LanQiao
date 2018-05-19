865832f8602447139fcfcfa69194ddd09ba5551a
paoqi1997 <30751379+paoqi1997@users.noreply.github.com>
2018-05-19 17:58:58 +0800

    入门训练

---
+++ 入门训练/BEGIN-01.cpp
@@ -0,0 +1,18 @@
+/**
+ * 入门训练 A+B问题
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b << endl;
+
+    return 0;
+}
---
+++ 入门训练/BEGIN-02.cpp
@@ -0,0 +1,18 @@
+/**
+ * 入门训练 序列求和
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+
+    cin >> n;
+
+    cout << n * (n + 1) / 2 << endl;
+
+    return 0;
+}
